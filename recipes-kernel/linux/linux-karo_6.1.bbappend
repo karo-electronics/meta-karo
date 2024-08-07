karo-electronics/meta-karo

commit 997d2df96b939ccbaa956b504044feb6b3236b30

linux: cleanup defconfigs

recipes-kernel/linux/linux-karo-6.1/cfg/stm32mp15.cfg
@@ -1,3 +1,6 @@
 # CONFIG_MACH_STM32MP13 is not set
-CONFIG_MACH_STM32MP13 is not set
-CONFIG_MACH_STM32MP157=y
+CONFIG_ARM_STM32_CPUIDLE=y
+CONFIG_HWSPINLOCK=y
+CONFIG_HWSPINLOCK_STM32=y
+CONFIG_STM32_IPCC=y
+CONFIG_DRM_PANEL_RPI_7INCH=y

recipes-kernel/linux/linux-karo-6.1/stm32mp1/defconfig
@@ -37,7 +37,6 @@ CONFIG_CPUFREQ_DT=y
 CONFIG_ARM_SCMI_CPUFREQ=y
 CONFIG_CPU_IDLE=y
 CONFIG_ARM_PSCI_CPUIDLE=y
-CONFIG_ARM_STM32_CPUIDLE=y
 CONFIG_VFP=y
 CONFIG_NEON=y
 CONFIG_PM_AUTOSLEEP=y
@@ -229,7 +228,6 @@ CONFIG_DRM_STM=y
 CONFIG_DRM_STM_DSI=y
 CONFIG_DRM_PANEL_LVDS=y
 CONFIG_DRM_PANEL_SIMPLE=y
-CONFIG_DRM_PANEL_RPI_7INCH=y
 CONFIG_DRM_DISPLAY_CONNECTOR=y
 CONFIG_DRM_LVDS_CODEC=y
 CONFIG_DRM_SIMPLE_BRIDGE=y
@@ -348,10 +346,7 @@ CONFIG_STM32_MDMA=y
 # CONFIG_VIRTIO_MENU is not set
 # CONFIG_VHOST_MENU is not set
 CONFIG_COMMON_CLK_SCMI=y
-CONFIG_HWSPINLOCK=y
-CONFIG_HWSPINLOCK_STM32=y
 CONFIG_CLKSRC_STM32_LP=y
-CONFIG_STM32_IPCC=y
 # CONFIG_IOMMU_SUPPORT is not set
 CONFIG_REMOTEPROC=y
 CONFIG_REMOTEPROC_CDEV=y

commit 63ef8357827afbcacab317cea8a81df3e3702bc7

qsmp-1351: linux: make kernel bootable again
Commit 997d2df96b93 ("linux: cleanup defconfigs") disabled
CONFIG_MACH_STM32MP157 for the stm32mp13 machines. This disabled some
vital functionality due to missing dependencies on
CONFIG_MACH_STM32MP13.

Add the missing dependencies to the Kconfig files and update the
defconfig and cfg fragments.

Fixes: 997d2df96b93 ("linux: cleanup defconfigs")

recipes-kernel/linux/linux-karo-6.1/cfg/stm32mp13.cfg
@@ -1,1 +1,2 @@
 # CONFIG_MACH_STM32MP157 is not set
+# CONFIG_ARM_STM32_CPUFREQ is not set

recipes-kernel/linux/linux-karo-6.1/cfg/stm32mp15.cfg
@@ -1,5 +1,6 @@
 # CONFIG_MACH_STM32MP13 is not set
-CONFIG_ARM_STM32_CPUIDLE=y
+CONFIG_CPUFREQ_DT=y
+CONFIG_ARM_SCMI_CPUFREQ=y
 CONFIG_HWSPINLOCK=y
 CONFIG_HWSPINLOCK_STM32=y
 CONFIG_STM32_IPCC=y

recipes-kernel/linux/linux-karo-6.1/stm32mp1/defconfig
@@ -33,10 +33,9 @@ CONFIG_CPU_FREQ_GOV_POWERSAVE=y
 CONFIG_CPU_FREQ_GOV_USERSPACE=y
 CONFIG_CPU_FREQ_GOV_ONDEMAND=y
 CONFIG_CPU_FREQ_GOV_CONSERVATIVE=y
-CONFIG_CPUFREQ_DT=y
-CONFIG_ARM_SCMI_CPUFREQ=y
 CONFIG_CPU_IDLE=y
 CONFIG_ARM_PSCI_CPUIDLE=y
+CONFIG_ARM_STM32_CPUIDLE=y
 CONFIG_VFP=y
 CONFIG_NEON=y
 CONFIG_PM_AUTOSLEEP=y

recipes-kernel/linux/linux-karo-6.1/stm32mp1/dts/stm32mp135c-qsmp-1351.dts
@@ -73,6 +73,10 @@
 	};
 };
 
+&cpu0 {
+	clock-frequency = <65000000>;
+};
+
 &cryp {
 	status = "okay";
 };

recipes-kernel/linux/linux-karo-6.1/stm32mp1/patches/stm32mp157-kconfig-dep-bugfix.patch
@@ -1,0 +1,65 @@
+Index: linux-qsmp-1351/arch/arm/Kconfig.debug
+===================================================================
+--- linux-qsmp-1351.orig/arch/arm/Kconfig.debug
++++ linux-qsmp-1351/arch/arm/Kconfig.debug
+@@ -1273,7 +1273,7 @@ choice
+ 
+ 	config STM32MP1_DEBUG_UART
+ 		bool "Use STM32MP1 UART for low-level debug"
+-		depends on MACH_STM32MP157
++		depends on MACH_STM32MP157 || MACH_STM32MP13
+ 		select DEBUG_STM32_UART
+ 		help
+ 		  Say Y here if you want kernel low-level debugging support
+Index: linux-qsmp-1351/drivers/cpuidle/Kconfig.arm
+===================================================================
+--- linux-qsmp-1351.orig/drivers/cpuidle/Kconfig.arm
++++ linux-qsmp-1351/drivers/cpuidle/Kconfig.arm
+@@ -95,7 +95,7 @@ config ARM_EXYNOS_CPUIDLE
+ 
+ config ARM_STM32_CPUIDLE
+ 	bool "Cpu Idle Driver for the STM32 processors"
+-	depends on MACH_STM32MP157
++	depends on MACH_STM32MP157 || MACH_STM32MP13
+ 	select DT_IDLE_STATES
+ 	select ARCH_NEEDS_CPU_IDLE_COUPLED if SMP
+ 	help
+Index: linux-qsmp-1351/drivers/soc/st/Kconfig
+===================================================================
+--- linux-qsmp-1351.orig/drivers/soc/st/Kconfig
++++ linux-qsmp-1351/drivers/soc/st/Kconfig
+@@ -3,15 +3,14 @@ if ARCH_STM32
+ config STM32_HDP
+ 	bool "STMicroelectronics STM32MP157 Hardware Debug Port (HDP) pin control"
+ 	depends on MACH_STM32MP157 || MACH_STM32MP13
+-	default n if MACH_STM32MP157 || MACH_STM32MP13
+ 	help
+ 	  The Hardware Debug Port allows the observation of internal signals. By using multiplexers,
+ 	  up to 16 signals for each of 8-bit output can be observed.
+ 
+ config STM32_PM_DOMAINS
+ 	bool "STM32 PM domains"
+-	depends on MACH_STM32MP157
++	depends on MACH_STM32MP157 || MACH_STM32MP13
+ 	select PM_GENERIC_DOMAINS
+-	default y if MACH_STM32MP157
++	default y
+ 
+ endif # ARCH_STM32
+Index: linux-qsmp-1351/drivers/thermal/st/Kconfig
+===================================================================
+--- linux-qsmp-1351.orig/drivers/thermal/st/Kconfig
++++ linux-qsmp-1351/drivers/thermal/st/Kconfig
+@@ -18,10 +18,9 @@ config ST_THERMAL_MEMMAP
+ 
+ config STM32_THERMAL
+ 	tristate "Thermal framework support on STMicroelectronics STM32 series of SoCs"
+-	depends on MACH_STM32MP157
++	depends on MACH_STM32MP157 || MACH_STM32MP13
+ 	default y
+ 	help
+ 	  Support for thermal framework on STMicroelectronics STM32 series of
+ 	  SoCs. This thermal driver allows to access to general thermal framework
+-	  functionalities and to access to SoC sensor functionalities. This
+-	  configuration is fully dependent of MACH_STM32MP157.
++	  functionalities and to access to SoC sensor functionalities.

recipes-kernel/linux/linux-karo_6.1.bbappend
@@ -51,3 +51,7 @@ SRC_URI:append:stm32mp15 = " \
         file://patches/0017-spi-nand-dma-map-bugfix.patch \
         file://patches/0019-fdt5x06-dma-bugfix.patch \
 "
+
+SRC_URI:append:stm32mp13 = " \
+        file://patches/stm32mp157-kconfig-dep-bugfix.patch \
+"